7b252874a6a19f4f6c9428d08cd18af9e98a7e92
lindiwe09 <dokotelal09@gmail.com>
2023-03-27 09:27:41 +0200

    add-hero-section

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,21 +3,19 @@ import  React from "@heroicons/react/24/outline";
 export default function HeroSection() {
     
     return (
-      <>
-    <div className="flex h-screen ">
-    
-      <div className="mx-10 max-w-2xl py-32 sm:py-48 lg:py-8">
-        <h1 className="text-6xl text-left font-bold tracking-tight text-gray-900 sm:text-6xl">
+      <div className="flex">
+        <div className="mx-10 max-w-2xl py-32 sm:py-48 lg:py-8">
+          <h1 className="text-6xl text-left font-bold tracking-tight text-gray-900 sm:text-6xl">
               Business <br></br>invoicing <br></br>software, you'll<br></br> love!
-        </h1>
+          </h1>
           <p className="mt-6 text-lg leading-8 text-yellow-400">
               LekkerInvoice is, simple, modern and reliable
           </p>
-          
           <div className="flex justify-start py-3">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  className="w-6 h-6 fill-emerald-300">
                 <path fillRule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z" clipRule="evenodd" />
-            </svg><p className="text-slate-700">Invoice with your team</p>
+            </svg>
+            <p className="text-slate-700">Invoice with your team</p>
           </div>
           <div className="flex justify-start py-3"> 
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" className="w-6 h-6 fill-emerald-300">
@@ -68,9 +66,9 @@ export default function HeroSection() {
             alt=""
           />
         </div>
-          </div>
+      </div>
         
-      </>
+      
     
   );
 }

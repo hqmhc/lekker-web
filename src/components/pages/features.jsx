f742c37526ca425bafc28432f121c597aad2317b
respect17 <respectmurimi2000@gmail.com>
2023-04-11 01:53:19 +0200

    imporove features page

--- src/components/pages/features.jsx
+++ src/components/pages/features.jsx
@@ -1,81 +1,131 @@
+import Logo from "../connect.png";
+import Connect from "../send.png";
 
 export const Features = () => {
  
     return (
-       <div className="border border-2 border-gray-300">
-          <div className="flex justify-center">
-  <div className=" flex justify-center  flex-col py-8">
-      <div className="">
-  <h2 class="text-base text-blue-500 font-semibold tracking-wide uppercase">
-                  The lekker life</h2>
+      
+  <div className="px-5 py-10">
+  
+        <div class="overflow-hidden bg-white  sm:py-10  px-3">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+            <div class="lg:ml-auto lg:pl-4 lg:pt-4">
+              <div class="lg:max-w-lg">
+                <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+                <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+                <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+                <dl class="mt-10 max-w-xl space-y-8 text-base leading-7 text-gray-600 lg:max-w-none">
+                  <div class="relative pl-9">
+                    <dt class="inline font-semibold text-gray-900">
+                      <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
+                      </svg>
+                      Push to deploy.
+                    </dt>
+                    <dd class="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
                   </div>
-  
+      
+                  <div class="relative pl-9">
+                    <dt class="inline font-semibold text-gray-900">
+                      <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
+                      </svg>
+                      SSL certificates.
+                    </dt>
+                    <dd class="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</dd>
+                  </div>
+      
+                  <div class="relative pl-9">
+                    <dt class="inline font-semibold text-gray-900">
+                      <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
+                        <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
+                      </svg>
+                      Database backups.
+                    </dt>
+                    <dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</dd>
+                  </div>
+                </dl>
+              </div>
+            </div>
+            <div class="flex items-start justify-end lg:order-first px-5 h--6">
+            <img class=" bg-white object-cover  rounded-md border border-2 " src={Logo}/>
+             </div>
+          </div>
+        </div>
+      </div>
+      
+      <div class="bg-white py-10 sm:py-10">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto max-w-2xl lg:mx-0">
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">All-in-one platform</h2>
+      <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.</p>
+    </div>
+    <dl class="mx-auto mt-16 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 text-base leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      <div>
+        <dt class="font-semibold text-gray-900">Push to deploy.</dt>
+        <dd class="mt-1 text-gray-600">Aut illo quae. Ut et harum ea animi natus. Culpa maiores et sed sint et magnam exercitationem quia. Ullam voluptas nihil vitae dicta molestiae et. Aliquid velit porro vero.</dd>
+      </div>
+
+      <div>
+        <dt class="font-semibold text-gray-900">SSL certificates.</dt>
+        <dd class="mt-1 text-gray-600">Mollitia delectus a omnis. Quae velit aliquid. Qui nulla maxime adipisci illo id molestiae. Cumque cum ut minus rerum architecto magnam consequatur. Quia quaerat minima.</dd>
+      </div>
+
+      <div>
+        <dt class="font-semibold text-gray-900">Simple queues.</dt>
+        <dd class="mt-1 text-gray-600">Aut repellendus et officiis dolor possimus. Deserunt velit quasi sunt fuga error labore quia ipsum. Commodi autem voluptatem nam. Quos voluptatem totam.</dd>
+      </div>
+
+      <div>
+        <dt class="font-semibold text-gray-900">Advanced security.</dt>
+        <dd class="mt-1 text-gray-600">Magnam provident veritatis odit. Vitae eligendi repellat non. Eum fugit impedit veritatis ducimus. Non qui aspernatur laudantium modi. Praesentium rerum error deserunt harum.</dd>
+      </div>
+
+      <div>
+        <dt class="font-semibold text-gray-900">Powerful API.</dt>
+        <dd class="mt-1 text-gray-600">Sit minus expedita quam in ullam molestiae dignissimos in harum. Tenetur dolorem iure. Non nesciunt dolorem veniam necessitatibus laboriosam voluptas perspiciatis error.</dd>
+      </div>
+
+      <div>
+        <dt class="font-semibold text-gray-900">Database backups.</dt>
+        <dd class="mt-1 text-gray-600">Ipsa in earum deserunt aut. Quos minus aut animi et soluta. Ipsum dicta ut quia eius. Possimus reprehenderit iste aspernatur ut est velit consequatur distinctio.</dd>
+      </div>
+    </dl>
   </div>
+</div>
+
+
+      <div class="overflow-hidden bg-white  sm:py-5">
+  <div class="mx-auto max-w-7xl px-6 lg:px-8">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start">
+      <div class="lg:pr-4 lg:pt-4">
+        <div class="lg:max-w-lg">
+          <h2 class="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2>
+          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">A better workflow</p>
+          <p class="mt-6 text-lg leading-8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
+          <div class="mt-8">
+            <a href="#" class="inline-flex rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get started</a>
+          </div>
+          <figure class="mt-16 border-l border-gray-200 pl-8 text-gray-600">
+            <blockquote class="text-base leading-7">
+              <p>“Vel ultricies morbi odio facilisi ultrices accumsan donec lacus purus. Lectus nibh ullamcorper ac dictum justo in euismod. Risus aenean ut elit massa. In amet aliquet eget cras. Sem volutpat enim tristique.”</p>
+            </blockquote>
+            <figcaption class="mt-6 flex gap-x-4 text-sm leading-6">
+              <img src="https://images.unsplash.com/photo-1509783236416-c9ad59bae472?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=1024&h=1024&q=80" alt="" class="h-6 w-6 flex-none rounded-full"/>
+              <div><span class="font-semibold text-gray-900">Maria Hill</span> – Marketing Manager</div>
+            </figcaption>
+          </figure>
+        </div>
+      </div>
+      <img class=" bg-white object-cover  rounded-md border border-2 " src={Connect}/>   </div>
   </div>
-  
-  <div class=" bg-white">
-      <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div class="lg:text-center">
-             
-                  <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">A better way to do your invoicing</p>
-                  <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Buy back valuable time for you and your customers. Remove the back-and-forth in raising invoices, confirming payments and generating receipts.</p></div><div class="mt-10"><dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-                      <div class=" px-5 relative shadow-xl rounded-md py-20">
-                          <dt>
-                              <div class=" absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9"></path>
-                                  </svg>
-                                  </div>
-                                  <p class="ml-16 text-lg leading-6 font-medium text-gray-900 underline">Invoice with your team</p>
-                                  </dt>
-                                  <dd class="mt-2 ml-16 text-base text-gray-500">
-                                      Work together as a team across departments and branches. Invite collaborators to create invoices together with you.</dd>
-                                      </div>
-  
-                                      
-                                      <div class=" px-5 relative shadow-xl rounded-md py-20">
-                                          <dt>
-                                              <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3">
-                                                  </path></svg>
-                                                  </div>
-                                                  <p class="ml-16 text-lg leading-6 font-medium text-gray-900 underline">Connect your Yoco or PayFast account</p>
-                                                  </dt><dd class="mt-2 ml-16 text-base text-gray-500">Invoice payments are processed with your Yoco or PayFast account. Get paid securely with the brands your customers know and trust.</dd></div>
-                                                 
-                                                 
-                                                 
-                                                  <div class="px-5 relative shadow-xl  rounded-md py-20">
-                                                      <dt>
-                                                          <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M13 10V3L4 14h7v7l9-11h-7z"></path>
-                                                          </svg>
-                                                          </div>
-                                                          <p class="ml-16 text-lg leading-6 font-medium text-gray-900 underline">
-                                                              Confirm Payments Immediately</p>
-                                                              </dt>
-                                                              <dd class="mt-2 ml-16 text-base text-gray-500">
-                                                                  Card and EFT payments processed with Yoco or PayFast are confirmed immediately. There is no delay, nor possibility of EFT fraud.</dd>
-                                                                  </div>
-                                                                  <div class="px-5 relative shadow-xl rounded-md py-20"><dt>
-                                                                      <div class="absolute flex items-center justify-center h-12 w-12 rounded-md bg-blue-500 text-white">
-                                                                          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-6 w-6"><path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z"></path></svg></div><p class="ml-16 text-lg leading-6 font-medium text-gray-900 underline">Issue Receipts Automatically</p>
-                                                                          </dt><dd class="mt-2 ml-16 text-base text-gray-500">A receipt is issued automatically when payment is secured, and the invoice is marked as paid.</dd>
-                                                                          </div>
-                                                                          </dl>
-                                                                          </div>
-                                                                          </div>
-                                                                          </div>
-  </div> 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+</div>
+
+
+
+  </div>
   
   
       

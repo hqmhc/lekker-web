6a43f53be136beff8bc068d305d751844e084459
Lindiwe <dokotelal09@gmail.com>
2023-04-14 13:02:56 +0200

    give ratios to image and text on the home page

--- src/components/pages/home.jsx
+++ src/components/pages/home.jsx
@@ -1,20 +1,30 @@
 import  React from "@heroicons/react/24/outline";
-import Footer from "../Footer/footer";
+
 import { Features } from "./features";
 import Pricing from "./pricing";
 import Contact from "./contact";
+import Signup from "./signup";
 import Logo from "../react-logo.png";
+import Typewriter from "typewriter-effect";
 
 export default function Home() {
     
     return (
       <>
       <div className="flex border border-gray-300 ">
-        <div className="mx-10 max-w-2xl py-32 sm:py-48 lg:py-8 px-20">
-          <h1 className="text-6xl text-left font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Business <br></br>invoicing <br></br>software, you'll<br></br> love!
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-lime-700">
+        <div className="container max-w-2xl py-32 sm:py-48 lg:py-8 px-20 ">
+          <div className="aspect-w-4 aspect-h-1">
+          <span className=" text-6xl text-left font-bold tracking-tight text-gray-900 sm:text-6xl aspect-w-2 ">
+           <Typewriter
+            options={{
+            strings: ["Business invoicing software, you'll love!"],
+            autoStart: true,
+            loop: true,
+            }}
+            /> 
+           </span>
+           </div>
+          <p className="mt-14 text-lg leading-8 text-lime-700">
               LekkerInvoice is, simple, modern and reliable
           </p>
           <div className="flex justify-start py-2 space-x-3">
@@ -42,12 +52,12 @@ export default function Home() {
               <p className="text-slate-700">Issue Receipts Automatically</p>
           </div>
           <div className="mt-5 flex items-center justify-items-start ">
-            <a href="https://beta.lekkerinvoice.co.za/register" className="rounded-md w-60 h-10 bg-blue-400  text-xl  text-center text-white shadow-sm hover:bg-green-500 py-1 ">
+            <a href="https://beta.lekkerinvoice.co.za/invoices" className="rounded-full w-60 h-10 bg-blue-400  text-xl  text-center text-white shadow-sm hover:bg-green-500 ">
               Start for free
             </a>
           </div>
           <div className="flex -justify-start py-10 space-x-3">
-             <p className='text-2xl font-mono'> <span className="font-bold ">Proven </span>Expert</p>
+             <p className='text-2xl font-mono animate-bounce'> <span className="font-bold ">Proven </span>Expert</p>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"  className="w-6 h-6 text-2xl fill-yellow-300">
              <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
             </svg>
@@ -65,20 +75,17 @@ export default function Home() {
             </svg> 
           </div>
           </div>
-          <div className="relative w-52  flex-1 lg:block">
-         
-             <img class=" pt-10 pb-20  pr-10 h-40 w-full bg-white sm:h-80 lg:absolute lg:h-full rounded-md " src={Logo} alt=""/>
-        
-          
+          <div className="container relative aspect-h-2">
+         <img class="absolute inset-0 right-0 top-0 h-64 w-full bg-fixed object-cover sm:h-80 lg:absolute lg:h-full rounded-md border border-2" src={Logo}/>
         </div>
       </div>
       <Pricing/>
       <Features/>
       <div className="bg-green-300"></div>
       <div className="py-20">
-    
+      <Contact/> 
       </div>
-        <Footer/>
+    
       </>
     
   );

4e1840153f8a6c86392c6f77230c10edee78591c
Lindiwe <dokotelal09@gmail.com>
2023-04-18 13:05:54 +0200

    change title to LekkerInvoice not react-app

--- src/components/pages/features.jsx
+++ src/components/pages/features.jsx
@@ -139,7 +139,7 @@ export const Features = () => {
 <div class="bg-white py-24 sm:py-32">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:text-center">
-      <h2 class="text-base font-semibold leading-7 text-indigo-600">Invoice faster</h2>
+      <h2 class="text-base font-semibold leading-7 text-blue-400">Invoice faster</h2>
       <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Everything you need to know about LekkerInvoice</p>
       <p class="mt-6 text-lg leading-8 text-gray-600">Staying on top of your expenses is crucial to running a strong business. With LekkerInvoice, it's simpler than ever to track your reimbursable expenses and bill your clients right away.</p>
     </div>
